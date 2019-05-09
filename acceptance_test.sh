775019818bca05741fb5ab882f2c5d1655ff2b16
trandmore <trandmore@gmail.com>
2019-05-09 14:37:18 +0900

    Update acceptance_test.sh

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-test $(curl localhost:8090/sum?a=1\&b=2) -eq 3 || test $(curl $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}'bridge):8090/sum?a=1\$b=2) -eq 3
+test $(curl localhost:8090/sum?a=1\&b=2) -eq 3 || test $(curl $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3
+CALCULATOR_PORT=$(docker-compose port calculator 8090 | cut -d: -f2)
+test $(curl --silent localhost:$CALCULATOR_PORT/sum?a=10\&b=2) -eq 12
